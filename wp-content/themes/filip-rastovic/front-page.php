63e4677f36b85aa24e770a1a7916c1295858f2ba
filiporastovic@gmail.com <filiporastovic@gmail.com>
2019-01-01 12:24:43 +0100

    added a new item to portfolio

--- wp-content/themes/filip-rastovic/front-page.php
+++ wp-content/themes/filip-rastovic/front-page.php
@@ -170,6 +170,20 @@ get_header();
           <p class="text-muted">Development &amp; Maintenance</p>
         </div>
       </div>
+      <div data-aos="fade-up"  class="col-md-6 portfolio-item text-center">
+        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal10">
+          <div class="portfolio-hover">
+            <div class="portfolio-hover-content">
+              <i class="fa fa-plus fa-3x"></i>
+            </div>
+          </div>
+          <img class="img-fluid" src="<?php bloginfo('template_url'); ?>/assets/img/portfolio/filiprastovic-small.png" alt="">
+        </a>
+        <div class="portfolio-caption">
+          <h4>Freelancer Landing Page</h4>
+          <p class="text-muted">Design &amp; Development</p>
+        </div>
+      </div>
     </div>
     <div class="row">
       <div class="col-md-12 text-center">
@@ -781,6 +795,41 @@ get_header();
                 <li>Date: 2017</li>
                 <li>Client: Synico</li>
                 <li>Category: Development &amp; Maintenance</li>
+              </ul>
+              <button class="btn btn-primary" data-dismiss="modal" type="button">
+                <i class="fa fa-times"></i>
+                Close Project</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal 10 -->
+<div class="portfolio-modal modal fade" id="portfolioModal10" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="close-modal" data-dismiss="modal">
+        <div class="lr">
+          <div class="rl"></div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <div class="modal-body">
+              <!-- Project Details Go Here -->
+              <h2 class="text-uppercase">Freelancer Landing Page</h2>
+              <p class="item-intro text-muted">Design &amp; Development</p>
+              <img class="img-fluid d-block mx-auto" src="<?php bloginfo('template_url'); ?>/assets/img/portfolio/filiprastovic-small.png"
+                alt="">
+              <p>I have designed and developed this general purpose freelancer landing page for up own web design business. It has as high conversion rate and pretty smooth design.</p>
+              <ul class="list-inline">
+                <li>Date: 2018</li>
+                <li>Client: Myself</li>
+                <li>Category: Design &amp; Development</li>
               </ul>
               <button class="btn btn-primary" data-dismiss="modal" type="button">
                 <i class="fa fa-times"></i>
