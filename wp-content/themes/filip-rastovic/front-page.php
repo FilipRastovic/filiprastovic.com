f5f6c1326de3331648b372f27d57210ed96a87e4
Filip Rastovic <filiporastovic@gmail.com>
2019-06-01 15:41:03 +0200

    simplified homepage

--- wp-content/themes/filip-rastovic/front-page.php
+++ wp-content/themes/filip-rastovic/front-page.php
@@ -17,12 +17,19 @@ get_header();
 <header class="header">
   <div class="container">
     <div class="row">
-      <div class="col-md-7 mb-5--mobile">
+      <div class="col-md-7 mx-auto">
         <h1 class="section-heading"><small><?php _e( 'Crafting' , 'filip-rastovic' ); ?></small><br><?php _e( 'DELIGHTFUL EXPERIENCES', 'filip-rastovic' ); ?></h1>
         <hr class="">
         <p>I'm Filip, a <strong>freelance designer</strong> with an affection for web, user experience and product design.  I work with companies around the globe helping them build beautiful and functional software with a focus on conversions, UX, responsive design, performance & accessibility. I build, manage & implement design systems, animations & interactions. My hobbies are <a href="/blog" target="_blank">writing</a> , <a href="https://github.com/FilipRastovic" target="_blank">coding</a> and making <a href="http://stargazerstudio.net" target="_blank">video games</a>. See my portfolio or check out my <a href="<?php bloginfo('template_url'); ?>/assets/download/Filip_Rastovic_Resume.pdf" target="_blank">resume</a>.</p>
       </div>
-      <div class="col-md-4 offset-md-1 mb-5--mobile text-center">
+    </div>
+  </div>
+</header>
+
+<section class="side-project">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 mx-auto text-center">
         <figure>
           <img class="img-fluid radi-4" src="<?php bloginfo('template_url') ?>/assets/img/long-hair.jpg" alt="Filip Rastovic">
         </figure>
@@ -58,71 +65,6 @@ get_header();
             </a>
           </li>
         </ul>
-      </div>
-    </div>
-  </div>
-</header>
-
-<section class="read-my-blog">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-12 card">
-        <div class="row">
-          <div class="col-lg-9">
-            <h2>Read my blog</h2>
-            <ul class="latest-posts-list">
-                <?php
-                  $recent_posts = wp_get_recent_posts( array(
-                    'numberposts' => '3',
-                    'post_status' =>'publish',
-                  ) );
-
-                  foreach( $recent_posts as $recent ) {
-
-                  echo 
-
-                  '<li> ' . get_the_post_thumbnail( $recent["ID"],'thumbnail') . '
-                    <a href="' . get_permalink( $recent["ID"] ) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> 
-                  </li> ';
-
-                  }
-                ?>
-              </ul>
-          </div>
-          <div class="col-lg-3 justify-content-center align-self-center text-center">
-            <a href="/blog" class="btn btn-secondary btn-square">Read Blog</a href="/blog">
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-
-<section class="companies">
-  <div class="container">
-    <div class="row row-eq-height">
-      <div class="col-md-5 mb-3">
-        <div class="ext-center">
-          <h5>Side project - Video Game</h5>
-          <h2>Trial &amp; Terror</h2>
-          <hr>
-          <div class="embed-responsive embed-responsive-16by9 mb-4">
-              <iframe src="https://www.youtube.com/embed/IBT5_B90JJg?controls=0" class="embed-responsive-item" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
-          <div class="text-center">
-            <a href="https://newgrounds.com/portal/view/713592" target="_BLANK" class="btn btn-secondary btn-small">Play</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-5 offset-md-1 mb-3">
-        <div class="text-center">
-          <h5>Side Project - Website Design Goodies <strong>Coming Soon</strong></h5>
-          <h2>Clarity Enhanced</h2>
-          <hr>
-          <figure>
-              <img class="img-fluid" src="<?php bloginfo('template_url') ?>/assets/img/clarity-enhanced.png" alt="Photo">
-          </figure>
-        </div>
       </div>
     </div>
   </div>
