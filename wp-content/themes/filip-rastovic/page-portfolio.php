e8aa11fce982371f2237acc4f5e33c0bbfa19fea
Filip Rastovic <filiporastovic@gmail.com>
2019-06-14 09:59:58 +0200

    -another video is now responsive

--- wp-content/themes/filip-rastovic/page-portfolio.php
+++ wp-content/themes/filip-rastovic/page-portfolio.php
@@ -15,264 +15,306 @@ get_header();
 ?>
 
 <header class="header">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-8 mx-auto text-center">
-        <h1 class="section-heading">Portfolio</h1>
-        <hr class="my-4">
-        <p class="mb-5">Check out some of my work!</p>
-      </div>
-    </div>
-  </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 mx-auto text-center">
+				<h1 class="section-heading">Portfolio</h1>
+				<hr class="my-4">
+				<p class="mb-5">Check out some of my work!</p>
+			</div>
+		</div>
+	</div>
 </header>
 
 <section class="header">
-  <div class="container">
-  <div class="row gutter-bot">
-		<div class="col-md-6">
-			<h2 class="text-thin">Customr.app</h4>
-			<hr>
-			<p>
-				<ul>
-					<li>Information architecture</li>
-					<li>Wireframing</li>
-					<li>UI/UX</li>
-					<li>HTML/SCSS/JAVASCRIPT/PHP/Wordpress</li>
-				</ul>
-				<a href="/case-studies">Read the case study.</a>
-			</p>
-		</div>
-		<div class="col-md-6">
-			<div class="portfolio-box">
-				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/customr-small.png" alt="">
+	<div class="container">
+		<div class="row gutter-bot">
+			<div class="col-md-6">
+				<h2 class="text-thin">Customr.app</h4>
+					<hr>
+					<p>
+						<ul>
+							<li>Information architecture</li>
+							<li>Wireframing</li>
+							<li>UI/UX</li>
+							<li>HTML/SCSS/JAVASCRIPT/PHP/Wordpress</li>
+						</ul>
+						<a href="/case-studies">Read the case study.</a>
+					</p>
+			</div>
+			<div class="col-md-6">
+				<div class="portfolio-box">
+					<img class="img-fluid"
+						src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/customr-small.png" alt="">
+				</div>
 			</div>
 		</div>
-    </div>
-    <div class="row gutter-bot">
-		<div class="col-md-6">
-			<h2 class="text-thin">Bonsall Pool</h4>
-			<hr>
-			<p>
-				<ul>
-					<li>Maintainance</li>
-					<li>HTML/SCSS</li>
-					<li>BEM convention</li>
-				</ul>
-			</p>
-		</div>
-		<div class="col-md-6">
-			<div class="portfolio-box">
-				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/bonsal-small.png" alt="">
+		<div class="row gutter-bot">
+			<div class="col-md-6">
+				<h2 class="text-thin">Bonsall Pool</h4>
+					<hr>
+					<p>
+						<ul>
+							<li>Maintainance</li>
+							<li>HTML/SCSS</li>
+							<li>BEM convention</li>
+						</ul>
+					</p>
+			</div>
+			<div class="col-md-6">
+				<div class="portfolio-box">
+					<img class="img-fluid"
+						src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/bonsal-small.png" alt="">
+				</div>
 			</div>
 		</div>
-    </div>
-	<div class="row gutter-bot">
-		<div class="col-md-6">
-			<h2 class="text-thin">Landing Page</h4>
-			<hr>
-			<p>
-				<ul>
-				<li>Information architecture</li>
-					<li>Wireframing</li>
-					<li>UI/UX</li>
-					<li>HTML/SCSS/JAVASCRIPT/PHP/Wordpress</li>
-				</ul>
-			</p>
-		</div>
-		<div class="col-md-6">
-			<div class="portfolio-box">
-				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/filiprastovic-small.png" alt="">
+		<div class="row gutter-bot">
+			<div class="col-md-6">
+				<h2 class="text-thin">Landing Page</h4>
+					<hr>
+					<p>
+						<ul>
+							<li>Information architecture</li>
+							<li>Wireframing</li>
+							<li>UI/UX</li>
+							<li>HTML/SCSS/JAVASCRIPT/PHP/Wordpress</li>
+						</ul>
+					</p>
+			</div>
+			<div class="col-md-6">
+				<div class="portfolio-box">
+					<img class="img-fluid"
+						src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/filiprastovic-small.png"
+						alt="">
+				</div>
 			</div>
 		</div>
-    </div>
-	<div class="row gutter-bot">
-		<div class="col-md-6">
-			<h2 class="text-thin">Justo</h4>
-			<hr>
-			<p>
-				<ul>
-				<li>Information architecture</li>
-					<li>Wireframing</li>
-					<li>UI/UX</li>
-					<li>HTML/SCSS/JAVASCRIPT/PHP/Wordpress</li>
-				</ul>
-			</p>
+		<div class="row gutter-bot">
+			<div class="col-md-6">
+				<h2 class="text-thin">Justo</h4>
+					<hr>
+					<p>
+						<ul>
+							<li>Information architecture</li>
+							<li>Wireframing</li>
+							<li>UI/UX</li>
+							<li>HTML/SCSS/JAVASCRIPT/PHP/Wordpress</li>
+						</ul>
+					</p>
+			</div>
+			<div class="col-md-6">
+				<div class="portfolio-box">
+					<img class="img-fluid"
+						src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/justo-small.png" alt="">
+				</div>
+			</div>
 		</div>
-		<div class="col-md-6">
-			<div class="portfolio-box">
-				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/justo-small.png" alt="">
+		<div class="row gutter-bot">
+			<div class="col-md-6">
+				<h2 class="text-thin">Natours</h4>
+					<hr>
+					<p>
+						<ul>
+							<li>Semantic HTML</li>
+							<li>SCSS architecture</li>
+						</ul>
+					</p>
+			</div>
+			<div class="col-md-6">
+				<div class="portfolio-box">
+					<img class="img-fluid"
+						src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/natours-small.png"
+						alt="Portfolio Pics">
+				</div>
+			</div>
+		</div>
+		<div class="row gutter-bot">
+			<div class="col-md-6">
+				<h2 class="text-thin">Home Manufacturer</h4>
+					<hr>
+					<p>
+						<ul>
+							<li>Wireframes</li>
+							<li>Mockups/UX/UI</li>
+							<li>Implementation</li>
+						</ul>
+					</p>
+			</div>
+			<div class="col-md-6">
+				<div class="portfolio-box">
+					<img class="img-fluid"
+						src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/ipad.png"
+						alt="Portfolio Pics">
+				</div>
+			</div>
+		</div>
+		<div class="row gutter-bot">
+			<div class="col-md-6">
+				<h2 class="text-thin">Web Agency</h4>
+					<hr>
+					<p>
+						<ul>
+							<li>UX/UI</li>
+							<li>Maintainance &amp; Implementation</li>
+						</ul>
+					</p>
+			</div>
+			<div class="col-md-6">
+				<div class="portfolio-box">
+					<img class="img-fluid"
+						src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/synico-small.png"
+						alt="Portfolio Pics">
+				</div>
+			</div>
+		</div>
+		<div class="row gutter-bot">
+			<div class="col-md-6">
+				<h2 class="text-thin">Landing Page</h4>
+					<hr>
+					<p>
+						<ul>
+							<li>Wireframes</li>
+							<li>UX/UI</li>
+							<li>mplementation</li>
+						</ul>
+					</p>
+			</div>
+			<div class="col-md-6">
+				<div class="portfolio-box">
+					<img class="img-fluid"
+						src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/wadeLP-small.png"
+						alt="Portfolio Pics">
+				</div>
+			</div>
+		</div>
+		<div class="row gutter-bot">
+			<div class="col-md-6">
+				<h2 class="text-thin">Interactive Guides Software</h4>
+					<hr>
+					<p>
+						<ul>
+							<li>UX/UI</li>
+							<li>Design</li>
+						</ul>
+					</p>
+			</div>
+			<div class="col-md-6">
+				<div class="portfolio-box">
+					<img class="img-fluid"
+						src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/solutionbay-small.png"
+						alt="Portfolio Pics">
+				</div>
+			</div>
+		</div>
+		<div class="row gutter-bot">
+			<div class="col-md-6">
+				<h2 class="text-thin">eCommerce UI</h4>
+					<hr>
+					<p>
+						<ul>
+							<li>UX/UI</li>
+							<li>Design</li>
+							<li>Implementation</li>
+						</ul>
+					</p>
+			</div>
+			<div class="col-md-6">
+				<div class="portfolio-box">
+					<img class="img-fluid mb-3" src="<?php echo get_template_directory_uri() ?>/assets/img/ecom-1.jpg"
+						alt="Portfolio Pics">
+					<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/ecom-2.jpg"
+						alt="Portfolio Pics">
+				</div>
+			</div>
+		</div>
+		<div class="row gutter-bot">
+			<div class="col-md-3 mb-5">
+				<img class="img-fluid"
+					src="<?php echo get_template_directory_uri() ?>/assets/img/new-portfolio-imgs/jpg.jpg"
+					alt="Portfolio Pics">
+			</div>
+			<div class="col-md-3 mb-5">
+				<img class="img-fluid"
+					src="<?php echo get_template_directory_uri() ?>/assets/img/new-portfolio-imgs/customr.jpg"
+					alt="Portfolio Pics">
+			</div>
+			<div class="col-md-3 mb-5">
+				<img class="img-fluid"
+					src="<?php echo get_template_directory_uri() ?>/assets/img/new-portfolio-imgs/national.jpg"
+					alt="Portfolio Pics">
+			</div>
+			<div class="col-md-3 mb-5">
+				<img class="img-fluid"
+					src="<?php echo get_template_directory_uri() ?>/assets/img/new-portfolio-imgs/qwd.png"
+					alt="Portfolio Pics">
+			</div>
+		</div>
+		<div class="row gutter-bot">
+			<div class="col-md-12 text-center">
+				<h4>Generative Design ( JavaScript Pseudorandomness )</h4>
+				<hr>
+			</div>
+			<div class="col-md-6 mb-5">
+				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/gene.png"
+					alt="Generative img">
+			</div>
+			<div class="col-md-6 mb-5">
+				<img class="img-fluid"
+					src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/question.png"
+					alt="Generative img">
+			</div>
+			<div class="col-md-6 mb-5">
+				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/1.jpg"
+					alt="Generative img">
+			</div>
+			<div class="col-md-6 mb-5">
+				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/2.jpg"
+					alt="Generative img">
+			</div>
+			<div class="col-md-6 mb-5">
+				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/3.jpg"
+					alt="Generative img">
+			</div>
+			<div class="col-md-6 mb-5">
+				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/4.jpg"
+					alt="Generative img">
+			</div>
+			<div class="col-md-6 mb-5">
+				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/5.jpg"
+					alt="Generative img">
+			</div>
+			<div class="col-md-6 mb-5">
+				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/6.jpg"
+					alt="Generative img">
+			</div>
+		</div>
+		<div class="row gutter-bot">
+			<div class="col-md-12 text-center">
+				<h4 class="mb-4">My Video Game</h4>
+				<hr>
+			</div>
+			<div class="col-md-6 mx-auto">
+				<div class="embed-responsive embed-responsive-16by9">
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/IBT5_B90JJg" frameborder="0"
+						allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+						allowfullscreen>
+					</iframe>
+				</div>
+			</div>
+		</div>
+		<div class="row gutter-bot">
+			<div class="col-md-12 text-center">
+				<h4 class="mb-4">How I Create Websites Free eBook</h4>
+				<hr>
+			</div>
+			<div class="col-md-4 mx-auto">
+				<a
+					href="<?php echo get_template_directory_uri() ?>/assets/download/Filip Rastovic - Website Creation eBook.pdf"><img
+						class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/ebook.png"
+						alt="Ebook Cover"></a>
 			</div>
 		</div>
 	</div>
-	<div class="row gutter-bot">
-		<div class="col-md-6">
-			<h2 class="text-thin">Natours</h4>
-			<hr>
-			<p>
-				<ul>
-					<li>Semantic HTML</li>
-					<li>SCSS architecture</li>
-				</ul>
-			</p>
-		</div>
-		<div class="col-md-6">
-			<div class="portfolio-box">
-				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/natours-small.png" alt="Portfolio Pics">
-			</div>
-		</div>
-	</div>
-	<div class="row gutter-bot">
-		<div class="col-md-6">
-			<h2 class="text-thin">Home Manufacturer</h4>
-			<hr>
-			<p>
-				<ul>
-					<li>Wireframes</li>
-					<li>Mockups/UX/UI</li>
-					<li>Implementation</li>
-				</ul>
-			</p>
-		</div>
-		<div class="col-md-6">
-			<div class="portfolio-box">
-				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/ipad.png" alt="Portfolio Pics">
-			</div>
-		</div>
-	</div>
-	<div class="row gutter-bot">
-		<div class="col-md-6">
-			<h2 class="text-thin">Web Agency</h4>
-			<hr>
-			<p>
-				<ul>
-					<li>UX/UI</li>
-					<li>Maintainance &amp; Implementation</li>
-				</ul>
-			</p>
-		</div>
-		<div class="col-md-6">
-			<div class="portfolio-box">
-				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/synico-small.png" alt="Portfolio Pics">
-			</div>
-		</div>
-	</div>
-	<div class="row gutter-bot">
-		<div class="col-md-6">
-			<h2 class="text-thin">Landing Page</h4>
-			<hr>
-			<p>
-				<ul>
-					<li>Wireframes</li>
-					<li>UX/UI</li>
-					<li>mplementation</li>
-				</ul>
-			</p>
-		</div>
-		<div class="col-md-6">
-			<div class="portfolio-box">
-				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/wadeLP-small.png" alt="Portfolio Pics">
-			</div>
-		</div>
-	</div>
-	<div class="row gutter-bot">
-		<div class="col-md-6">
-			<h2 class="text-thin">Interactive Guides Software</h4>
-			<hr>
-			<p>
-				<ul>
-					<li>UX/UI</li>
-					<li>Design</li>
-				</ul>
-			</p>
-		</div>
-		<div class="col-md-6">
-			<div class="portfolio-box">
-				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/solutionbay-small.png" alt="Portfolio Pics">
-			</div>
-		</div>
-	</div>
-	<div class="row gutter-bot">
-		<div class="col-md-6">
-			<h2 class="text-thin">eCommerce UI</h4>
-			<hr>
-			<p>
-				<ul>
-					<li>UX/UI</li>
-					<li>Design</li>
-					<li>Implementation</li>
-				</ul>
-			</p>
-		</div>
-		<div class="col-md-6">
-			<div class="portfolio-box">
-				<img class="img-fluid mb-3" src="<?php echo get_template_directory_uri() ?>/assets/img/ecom-1.jpg" alt="Portfolio Pics">
-				<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/ecom-2.jpg" alt="Portfolio Pics">
-			</div>
-		</div>
-	</div>
-	<div class="row gutter-bot">
-		<div class="col-md-3 mb-5">
-			<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/new-portfolio-imgs/jpg.jpg" alt="Portfolio Pics">
-		</div>
-		<div class="col-md-3 mb-5">
-			<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/new-portfolio-imgs/customr.jpg" alt="Portfolio Pics">
-		</div>
-		<div class="col-md-3 mb-5">
-			<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/new-portfolio-imgs/national.jpg" alt="Portfolio Pics">
-		</div>
-		<div class="col-md-3 mb-5">
-			<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/new-portfolio-imgs/qwd.png" alt="Portfolio Pics">
-		</div>
-	</div>
-	<div class="row gutter-bot">
-		<div class="col-md-12 text-center">
-			<h4>Generative Design ( JavaScript Pseudorandomness )</h4>
-			<hr>
-		</div>
-		<div class="col-md-6 mb-5">
-			<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/gene.png" alt="Generative img">
-		</div>
-		<div class="col-md-6 mb-5">
-			<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/question.png" alt="Generative img">
-		</div>
-		<div class="col-md-6 mb-5">
-			<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/1.jpg" alt="Generative img">
-		</div>
-		<div class="col-md-6 mb-5">
-			<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/2.jpg" alt="Generative img">
-		</div>
-		<div class="col-md-6 mb-5">
-			<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/3.jpg" alt="Generative img">
-		</div>
-		<div class="col-md-6 mb-5">
-			<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/4.jpg" alt="Generative img">
-		</div>
-		<div class="col-md-6 mb-5">
-			<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/5.jpg" alt="Generative img">
-		</div>
-		<div class="col-md-6 mb-5">
-			<img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/portfolio/6.jpg" alt="Generative img">
-		</div>
-	</div>
-	<div class="row gutter-bot">
-		<div class="col-md-12 text-center">
-			<h4 class="mb-4">My Video Game</h4>
-			<hr>
-		</div>
-		<div class="col-md-6 mx-auto">
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/IBT5_B90JJg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-		</div>
-	</div>
-	<div class="row gutter-bot">
-		<div class="col-md-12 text-center">
-			<h4 class="mb-4">How I Create Websites Free eBook</h4>
-			<hr>
-		</div>
-		<div class="col-md-4 mx-auto">
-			<a href="<?php echo get_template_directory_uri() ?>/assets/download/Filip Rastovic - Website Creation eBook.pdf"><img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/assets/img/ebook.png" alt="Ebook Cover"></a>
-		</div>
-	</div>
-  </div>
 </section>
 
 
