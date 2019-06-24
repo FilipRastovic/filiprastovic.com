8f882fcb2e5f94b2628ee7cde3d35898205971cb
Filip Rastovic <filiporastovic@gmail.com>
2019-06-24 18:41:42 +0200

    -afiliates added

---
+++ wp-content/themes/filip-rastovic/afiliate-program.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Template Name: Afiliate Program
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ *
+ * @package filiprastovic
+ */
+
+get_header();
+
+$servername = "127.0.0.1";
+$username = "u111992089_isot";
+$password = "jXuETTKh5jHw";
+$dbname = "u111992089_isot";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$data = $_POST['afiliateName'] or $_REQUEST['afiliateName'];
+
+var_dump ( $data );
+
+$sql = "INSERT INTO afiliates ( Name ) VALUES ('$data')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$sql = "SELECT Name FROM afiliates";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Affiliate Name: " . $row["Name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+
+?>
