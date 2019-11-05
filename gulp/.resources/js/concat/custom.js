518ba692d4ab5fb8f9f3b1fb2afaa65360722591
Filip Rastovic <filiporastovic@gmail.com>
2019-11-05 21:20:11 +0100

    -Added more testimonials.

--- gulp/.resources/js/concat/custom.js
+++ gulp/.resources/js/concat/custom.js
@@ -118,8 +118,8 @@ function canvasApp() {
 			
 			//define gradient
 			grad = context.createRadialGradient(0,0,minR,0,0,maxR);
-			grad.addColorStop(1,"rgba(180,200,250, 0.1)");
-			grad.addColorStop(0,"rgba(180,200,250, 0.1)"); 
+			grad.addColorStop(1,"rgba(0,0,0, 0.01)");
+			grad.addColorStop(0,"rgba(0,0,0, 0.01)"); 
 			
 			var newCircle = {
 				centerX: -maxR,
@@ -319,50 +319,6 @@ function canvasApp() {
 }
 
 
-var flag;
-
-$( '#haircut' ).on( 'click', function() {
-
-	var string = $( '.filipo' ).attr( 'src' );
-
-	if ( string === '/wp-content/themes/filip-rastovic/assets/img/long-hair.jpg' ) {
-
-		string = '/wp-content/themes/filip-rastovic/assets/img/profilna.jpg';
-
-		$( '#haircut' ).text( 'Put hair back on!' );
-
-	} else {
-
-		string = '/wp-content/themes/filip-rastovic/assets/img/long-hair.jpg';
-
-		$( '#haircut' ).text( 'Click to cut my hair!' );
-
-	}
-
-	$( '.filipo' ).attr( 'src', string );
-
-});
-
-new WOW().init();
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 72)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
-
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
-
 function verificationForm(){
 	//jQuery time
 	var current_fs, next_fs, previous_fs; //fieldsets
@@ -477,6 +433,32 @@ verificationForm ();
 phoneNoselect ();
 nice_Select ();
 
+
+var flag;
+
+$( '#haircut' ).on( 'click', function() {
+
+	var string = $( '.filipo' ).attr( 'src' );
+
+	if ( string === '/wp-content/themes/filip-rastovic/assets/img/long-hair.jpg' ) {
+
+		string = '/wp-content/themes/filip-rastovic/assets/img/profilna.jpg';
+
+		$( '#haircut' ).text( 'Put hair back on!' );
+
+	} else {
+
+		string = '/wp-content/themes/filip-rastovic/assets/img/long-hair.jpg';
+
+		$( '#haircut' ).text( 'Click to cut my hair!' );
+
+	}
+
+	$( '.filipo' ).attr( 'src', string );
+
+});
+
+new WOW().init();
   // Collapse Navbar
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
@@ -488,4 +470,22 @@ nice_Select ();
   // Collapse now if page is not at top
   navbarCollapse();
   // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);});
+  $(window).scroll(navbarCollapse);
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 72)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });});
--- page-process.php
+++ page-process.php
@@ -201,7 +201,7 @@ get_header();
 	</div>
 </section>
 
-<section class="page-section testimonials">
+<!-- <section class="page-section testimonials">
 	<div class="container">
 		<div class="row justify-content-center">
 			<div class="col-lg-3 text-center wow animated fadeIn" data-wow-duration="2s" data-wow-delay=".2s">
@@ -211,6 +211,62 @@ get_header();
 				<p class="font-weight-light text-muted testimonial-paragraph text-left mb-4"><i>I needed someone who can design AND develop a landing page and upload it to my website. Filip did a great work in doing the above. It is pleasure working with Filip, he understands what needs to be done and does it in very quick and efficient way. For my next projects for landing pages I would would with him again. Thank you Filip!</i></p>
 				<p class="text-primary font-weight-bold mb-2 text-left">Maria Davidson</p>
 				<p class="font-weight-light text-muted text-left">Justo</p>
+			</div>
+		</div>
+	</div>
+</section> -->
+
+<section class="page-section testimonials">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 wow animated fadeIn" data-wow-duration="2s" data-wow-delay=".2s">
+				<h2 class="text-uppercase font-weight-bold mb-5">Client testimonials</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-4 mb-5 wow fadeIn animated" data-wow-duration="1s" data-wow-delay=".1s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.1s; animation-name: fadeIn;">
+				<p class="font-weight-light text-muted testimonial-paragraph text-left mb-4"><i>Filip has a unique combo of skills: design &amp; development. He proactively suggested some great ideas on how to make the website not just functional but esthetic. He created awesome visual mocks, iterated quickly based on my input and then coded all of it into a superb website. Throughout the process Filip was a charm to work with. All All in all great designer-developer with an outstanding sense for client service.</i></p>
+				<p class="text-primary font-weight-bold mb-2 text-left">Robert Molnar</p>
+				<p class="font-weight-light text-muted text-left">CEO Of Stratolytics</p>
+			</div>
+			<div class="col-lg-4 mb-5 wow fadeIn animated" data-wow-duration="1s" data-wow-delay=".1s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.1s; animation-name: fadeIn;">
+				<p class="font-weight-light text-muted testimonial-paragraph text-left mb-4">
+					I needed someone who can design AND develop a landing page and upload it to my website. Filip did a great work in doing the above. It is pleasure working with Filip, he understands what needs to be done and does it in very quick and efficient way.
+					For my next projects for landing pages I would would with him again.
+					Thank you Filip!
+				</p>
+				<p class="text-primary font-weight-bold mb-2 text-left">Maria Davidson</p>
+				<p class="font-weight-light text-muted text-left">Justo</p>
+			</div>
+			<div class="col-lg-4 mb-5 wow fadeIn animated" data-wow-duration="1s" data-wow-delay=".1s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.1s; animation-name: fadeIn;">
+				<p class="font-weight-light text-muted testimonial-paragraph text-left mb-4">
+					Filip was AMAZING! The only reason we are ending the contract is because he got too busy with bigger projects. I am sad to loose him as he is awesome. I would definitely hire again and again. If you have the opportunity to work with Filip, you should absolutely do it, he will make you proud!
+				</p>
+				<p class="text-primary font-weight-bold mb-2 text-left">Liam Shy</p>
+				<p class="font-weight-light text-muted text-left">Flight Collective</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-4 mb-5 wow fadeIn animated" data-wow-duration="1s" data-wow-delay=".1s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.1s; animation-name: fadeIn;">
+				<p class="font-weight-light text-muted testimonial-paragraph text-left mb-4">
+					Fillip understood the project, understood my needs and made it happen. Created a really beautiful landing page that is going to work well with my ad campaign.
+				</p>
+				<p class="text-primary font-weight-bold mb-2 text-left">Andrew Semaan</p>
+				<p class="font-weight-light text-muted text-left">Practice Cultivator</p>
+			</div>
+			<div class="col-lg-4 mb-5 wow fadeIn animated" data-wow-duration="1s" data-wow-delay=".1s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.1s; animation-name: fadeIn;">
+				<p class="font-weight-light text-muted testimonial-paragraph text-left mb-4">
+					Whaou ! working with Filip is just GREAT ! Everything is easy and simple. Thanks a lot for your great job, will have more job soon ;)
+				</p>
+				<p class="text-primary font-weight-bold mb-2 text-left">Battista Ravane</p>
+				<p class="font-weight-light text-muted text-left">USBFactory</p>
+			</div>
+			<div class="col-lg-4 mb-5 wow fadeIn animated" data-wow-duration="1s" data-wow-delay=".1s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.1s; animation-name: fadeIn;">
+				<p class="font-weight-light text-muted testimonial-paragraph text-left mb-4">
+					Working with Filip has been a very smooth process. He is quick and clear with communication and works through revisions easily and without any hiccups.
+				</p>
+				<p class="text-primary font-weight-bold mb-2 text-left">Mike Hutter</p>
+				<p class="font-weight-light text-muted text-left">ConvertMore</p>
 			</div>
 		</div>
 	</div>
