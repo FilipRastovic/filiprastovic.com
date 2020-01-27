19ab9a84ba6ead7533a85976ea74833c233d2d55
Filip Rastovic <filiporastovic@gmail.com>
2020-01-27 12:17:18 +0100

    -Changed req con header bck color.

--- gulp/.resources/js/concat/custom.js
+++ gulp/.resources/js/concat/custom.js
@@ -148,6 +148,34 @@ jQuery(document).ready(function($){/**
 		// }
 
     })();
+
+var flag;
+
+$( '.haircut' ).on( 'click', function() {
+
+	var string = $( '.filipo' ).attr( 'src' );
+
+	if ( string === '/wp-content/themes/filip-rastovic/assets/img/long-hair.jpg' ) {
+
+		string = '/wp-content/themes/filip-rastovic/assets/img/profilna.jpg';
+
+		$( '.haircut' ).text( 'Put hair back on!' );
+
+	} else {
+
+		string = '/wp-content/themes/filip-rastovic/assets/img/long-hair.jpg';
+
+		$( '.haircut' ).text( 'Click to cut my hair!' );
+
+	}
+
+	$( '.filipo' ).attr( 'src', string );
+
+});
+
+
+
+new WOW().init();
 window.addEventListener("load", windowLoadHandler, false);
 
 //for debug messages while testing code
@@ -468,168 +496,6 @@ function canvasApp() {
 	
 }
 
-
-var flag;
-
-$( '.haircut' ).on( 'click', function() {
-
-	var string = $( '.filipo' ).attr( 'src' );
-
-	if ( string === '/wp-content/themes/filip-rastovic/assets/img/long-hair.jpg' ) {
-
-		string = '/wp-content/themes/filip-rastovic/assets/img/profilna.jpg';
-
-		$( '.haircut' ).text( 'Put hair back on!' );
-
-	} else {
-
-		string = '/wp-content/themes/filip-rastovic/assets/img/long-hair.jpg';
-
-		$( '.haircut' ).text( 'Click to cut my hair!' );
-
-	}
-
-	$( '.filipo' ).attr( 'src', string );
-
-});
-
-
-
-new WOW().init();
-
-function verificationForm(){
-	//jQuery time
-	var current_fs, next_fs, previous_fs; //fieldsets
-	var left, opacity, scale; //fieldset properties which we will animate
-	var animating; //flag to prevent quick multi-click glitches
-
-	$(".next").click(function () {
-		if (animating) return false;
-		animating = true;
-
-		current_fs = $(this).parent();
-		next_fs = $(this).parent().next();
-
-		//activate next step on progressbar using the index of next_fs
-		$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-
-		//show the next fieldset
-		next_fs.show();
-		//hide the current fieldset with style
-		current_fs.animate({
-			opacity: 0
-		}, {
-			step: function (now, mx) {
-				//as the opacity of current_fs reduces to 0 - stored in "now"
-				//1. scale current_fs down to 80%
-				scale = 1 - (1 - now) * 0.2;
-				//2. bring next_fs from the right(50%)
-				left = (now * 50) + "%";
-				//3. increase opacity of next_fs to 1 as it moves in
-				opacity = 1 - now;
-				current_fs.css({
-					'transform': 'scale(' + scale + ')',
-					'position': 'absolute'
-				});
-				next_fs.css({
-					'left': left,
-					'opacity': opacity
-				});
-			},
-			duration: 800,
-			complete: function () {
-				current_fs.hide();
-				animating = false;
-			},
-			//this comes from the custom easing plugin
-			easing: 'easeInOutBack'
-		});
-	});
-
-	$(".previous").click(function () {
-		if (animating) return false;
-		animating = true;
-
-		current_fs = $(this).parent();
-		previous_fs = $(this).parent().prev();
-
-		//de-activate current step on progressbar
-		$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-
-		//show the previous fieldset
-		previous_fs.show();
-		//hide the current fieldset with style
-		current_fs.animate({
-			opacity: 0
-		}, {
-			step: function (now, mx) {
-				//as the opacity of current_fs reduces to 0 - stored in "now"
-				//1. scale previous_fs from 80% to 100%
-				scale = 0.8 + (1 - now) * 0.2;
-				//2. take current_fs to the right(50%) - from 0%
-				left = ((1 - now) * 50) + "%";
-				//3. increase opacity of previous_fs to 1 as it moves in
-				opacity = 1 - now;
-				current_fs.css({
-					'left': left
-				});
-				previous_fs.css({
-					'transform': 'scale(' + scale + ')',
-					'opacity': opacity
-				});
-			},
-			duration: 800,
-			complete: function () {
-				current_fs.hide();
-				animating = false;
-			},
-			//this comes from the custom easing plugin
-			easing: 'easeInOutBack'
-		});
-	});
-
-	$(".submit").click(function () {
-		return false;
-	})
-}; 
-
-//* Add Phone no select
-function phoneNoselect(){
-	if ( $('#msform').length ){   
-		$("#phone").intlTelInput(); 
-		$("#phone").intlTelInput("setNumber", "+880"); 
-	};
-}; 
-//* Select js
-function nice_Select(){
-	if ( $('.product_select').length ){ 
-		$('select').niceSelect();
-	};
-}; 
-/*Function Calls*/  
-verificationForm ();
-phoneNoselect ();
-nice_Select ();
-
-  // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
-
-  $( '.contact-link' ).on( 'click', function() {
-
-    $( '#exampleModal' ).modal( 'show' );
-
-  });
-    
 /*! Magnific Popup - v1.1.0 - 2016-02-20
 * http://dimsemenov.com/plugins/magnific-popup/
 * Copyright (c) 2016 Dmitry Semenov; */
@@ -2494,6 +2360,25 @@ nice_Select ();
 	 $(document).ready(function() {
 		$('.image-link').magnificPopup({type:'image'});
 	  });
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  $( '.contact-link' ).on( 'click', function() {
+
+    $( '#exampleModal' ).modal( 'show' );
+
+  });
+    
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -2512,6 +2397,121 @@ nice_Select ();
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
   });
+
+function verificationForm(){
+	//jQuery time
+	var current_fs, next_fs, previous_fs; //fieldsets
+	var left, opacity, scale; //fieldset properties which we will animate
+	var animating; //flag to prevent quick multi-click glitches
+
+	$(".next").click(function () {
+		if (animating) return false;
+		animating = true;
+
+		current_fs = $(this).parent();
+		next_fs = $(this).parent().next();
+
+		//activate next step on progressbar using the index of next_fs
+		$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+		//show the next fieldset
+		next_fs.show();
+		//hide the current fieldset with style
+		current_fs.animate({
+			opacity: 0
+		}, {
+			step: function (now, mx) {
+				//as the opacity of current_fs reduces to 0 - stored in "now"
+				//1. scale current_fs down to 80%
+				scale = 1 - (1 - now) * 0.2;
+				//2. bring next_fs from the right(50%)
+				left = (now * 50) + "%";
+				//3. increase opacity of next_fs to 1 as it moves in
+				opacity = 1 - now;
+				current_fs.css({
+					'transform': 'scale(' + scale + ')',
+					'position': 'absolute'
+				});
+				next_fs.css({
+					'left': left,
+					'opacity': opacity
+				});
+			},
+			duration: 800,
+			complete: function () {
+				current_fs.hide();
+				animating = false;
+			},
+			//this comes from the custom easing plugin
+			easing: 'easeInOutBack'
+		});
+	});
+
+	$(".previous").click(function () {
+		if (animating) return false;
+		animating = true;
+
+		current_fs = $(this).parent();
+		previous_fs = $(this).parent().prev();
+
+		//de-activate current step on progressbar
+		$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+		//show the previous fieldset
+		previous_fs.show();
+		//hide the current fieldset with style
+		current_fs.animate({
+			opacity: 0
+		}, {
+			step: function (now, mx) {
+				//as the opacity of current_fs reduces to 0 - stored in "now"
+				//1. scale previous_fs from 80% to 100%
+				scale = 0.8 + (1 - now) * 0.2;
+				//2. take current_fs to the right(50%) - from 0%
+				left = ((1 - now) * 50) + "%";
+				//3. increase opacity of previous_fs to 1 as it moves in
+				opacity = 1 - now;
+				current_fs.css({
+					'left': left
+				});
+				previous_fs.css({
+					'transform': 'scale(' + scale + ')',
+					'opacity': opacity
+				});
+			},
+			duration: 800,
+			complete: function () {
+				current_fs.hide();
+				animating = false;
+			},
+			//this comes from the custom easing plugin
+			easing: 'easeInOutBack'
+		});
+	});
+
+	$(".submit").click(function () {
+		return false;
+	})
+}; 
+
+//* Add Phone no select
+function phoneNoselect(){
+	if ( $('#msform').length ){   
+		$("#phone").intlTelInput(); 
+		$("#phone").intlTelInput("setNumber", "+880"); 
+	};
+}; 
+//* Select js
+function nice_Select(){
+	if ( $('.product_select').length ){ 
+		$('select').niceSelect();
+	};
+}; 
+/*Function Calls*/  
+verificationForm ();
+phoneNoselect ();
+nice_Select ();
+
 $(document).ready(function(){
 	$('[data-toggle="tooltip"]').tooltip();
   });});
