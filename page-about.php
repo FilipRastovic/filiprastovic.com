fd6325cb821efa810b56cea8ae133f33f9ba10ef
Filip Rastovic <filiporastovic@gmail.com>
2020-04-08 15:24:55 +0200

    -Changed about me section.

--- page-about.php
+++ page-about.php
@@ -60,25 +60,21 @@ get_header();
 	<div class="container">
 		<div class="row">
 			<div class="col-md-6">
-				<p class="font-weight-light">Hi, you're a small to medium-sized business that relies on your web presence and paid advertising in order to generate leads? You're overwhelmed by too much choice and finding the right person to take care of your web related graphic design problems? I can help.
+				<h2 class="font-weight-bold">About me</h2>
 
-					I'm Filip, a <span class="font-weight-bold">graphic designer</span> with an academic background in computer science and engineering. I understand it's a dice-roll to trust the design of your most profitable customer touchpoints to a designer from Eastern Europe, but my multi-disciplinary skillset can help you strategize, plan and execute successful solutions for your next marketing campaign.
-					
-					The plan I have for you is the following: leverage the latest technologies when building websites, landing pages, and app UI, adopt a scientific approach to problem-solving, develop a hypothesis, test and then optimize.
-					
-				</p>
+				<p class="font-weight-light">Hi, you're a small to medium-sized business that relies on your web presence and paid advertising in order to generate leads? You're overwhelmed by too much choice and finding the right person to take care of your web related graphic design problems? I can help.</p>
+				<p class="font-weight-light">I'm Filip, a graphic designer with an academic background in computer science and engineering. I understand it's a dice-roll to trust the design of your most profitable customer touchpoints to a designer from Eastern Europe, but my multi-disciplinary skillset can help you strategize, plan and execute successful solutions for your next marketing campaign.</p>
+				<p class="font-weight-light">The plan I propose to my clients is a proven methodology I've developed over the years working in the industry as a web designer focused on increasing conversion rates and optimizing websites.</p>
+				
 			</div>
 			<div class="col-md-6">
 				<p class="font-weight-light">
-					 When it comes to web design - starting with a simple user interface and then slightly tweaking it, driving traffic to it and then improving based on insights collected is the only way to develop useful user interfaces.
-
-					So let's get in touch. Don't hesitate to send me a message, <span class="font-weight-bold">I'm here to consult you.</span> If you're having doubts, feel free to watch the video on my profile about the landing page design process and read past client reviews on my profile. Also, check out my portfolio at clarity-enhanced.com/portfolio
-					
-					We will work together to achieve successful results, generate more leads, reduce the cost of customer acquisition and avoid wasting your budget on driving traffic to interfaces that just don't work.
-					
-					Best
-					Filip
+				So let's get in touch. Don't hesitate to send me a message, I'm here to consult you. If you're having doubts, feel free to watch the video on my profile about the landing page design process and read past client reviews on my profile. 
 				</p>
+				<p class="font-weight-light">We will work together to achieve successful results, generate more leads, reduce the cost of customer acquisition and avoid wasting your budget on driving traffic to interfaces that just don't work. <br>
+
+Best <br>
+Filip</p>
 			</div>
 		</div>
 	</div>
